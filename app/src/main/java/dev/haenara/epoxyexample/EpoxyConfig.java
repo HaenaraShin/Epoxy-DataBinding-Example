9cbbe5fe920c03c6ea626f510062e83bd1b71d8c
HaenaraShin <hamster12345@gmail.com>
2020-09-28 00:04:37 +0900

    refactor(common): EpoxyConfig.java --> EpoxyConfig.kt

--- app/src/main/java/dev/haenara/epoxyexample/EpoxyConfig.java
+++
@@ -1,8 +0,0 @@
-//package dev.haenara.epoxyexample;
-//
-//import com.airbnb.epoxy.EpoxyDataBindingLayouts;
-//import com.airbnb.epoxy.PackageModelViewConfig;
-//
-//@PackageModelViewConfig(rClass = R.class)
-//@EpoxyDataBindingLayouts(R.layout.item_sample)
-//interface EpoxyConfig {}
---
+++ app/src/main/java/dev/haenara/epoxyexample/EpoxyConfig.kt
@@ -0,0 +1,8 @@
+//package dev.haenara.epoxyexample
+//
+//import com.airbnb.epoxy.EpoxyDataBindingLayouts
+//import com.airbnb.epoxy.PackageModelViewConfig
+//
+//@PackageModelViewConfig(rClass = R::class)
+//@EpoxyDataBindingLayouts(R.layout.item_sample)
+//internal interface EpoxyConfig 
